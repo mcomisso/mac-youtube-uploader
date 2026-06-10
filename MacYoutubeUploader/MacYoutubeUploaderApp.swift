cbd1f31c20c1b2dc042ad10d3647578ade6eac15
Matteo Comisso <matteo.comisso@me.com>
2026-06-10 15:45:00 +0100

    Initial Commit

---
+++ MacYoutubeUploader/MacYoutubeUploaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacYoutubeUploaderApp.swift
+//  MacYoutubeUploader
+//
+//  Created by Matteo Comisso on 10/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MacYoutubeUploaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacYoutubeUploaderTests/MacYoutubeUploaderTests.swift
@@ -0,0 +1,19 @@
+//
+//  MacYoutubeUploaderTests.swift
+//  MacYoutubeUploaderTests
+//
+//  Created by Matteo Comisso on 10/06/2026.
+//
+
+import Testing
+@testable import MacYoutubeUploader
+
+struct MacYoutubeUploaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
